cc3cd2c3c5c3e542ec9d384cfcce3a905eb26d96
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2019-12-15 11:04:55 -0800

    Add an IntCode computer utility for Haskell.

--- 2019/p13/src/Lib.hs
+++ 2019/p13/src/Lib.hs
@@ -3,22 +3,24 @@ module Lib
     , parse2
     , answer1
     , answer2
-    , run
     ) where
 
+--Note!  This one has been modified a lot as a test bed for a generalized IntCode computer
 --import Prelude hiding (lookup)
 --import Data.ByteString (ByteString)
 import Data.Map (Map)
 
+import AdventOfCode.IntCode
+       (Computer, WaitState(..), consume, initialize, parseInsts, step)
 import AdventOfCode.Util
        (applyNTimes, elmTrace, prettyPrintPointMap)
 import Control.Applicative ((<*>), liftA2)
 import Control.Monad (replicateM)
 import Control.Monad.Loops (unfoldWhileM)
-import Control.Monad.State.Lazy (State, evalState, get, runState)
 import Crypto.Hash.MD5 ()
 import qualified Data.ByteString as BS ()
 import Data.ByteString.UTF8 ()
+import Data.Foldable (toList)
 import Data.List (cycle, permutations)
 import Data.List.Split (chunksOf, splitOn)
 import qualified Data.Map as Map
@@ -31,153 +33,11 @@ import Data.Set (Set)
 import qualified Data.Set as Set
        (delete, fromList, insert, member, singleton, size, toList)
 
-listToIndexMap :: [a] -> Map Integer a
-listToIndexMap =
-    fst . foldl (\(m, i) v -> (Map.insert i v m, i + 1)) (mempty, 0)
-
 parse1 :: String -> Map Integer Integer
-parse1 = listToIndexMap . fmap read . splitOn ","
+parse1 = parseInsts
 
 parse2 :: String -> _
 parse2 = parse1
-
-data Mode
-    = Immediate
-    | Position
-    | Relative
-    deriving (Show)
-
-decodeOp :: Integer -> (Mode, Mode, Mode, Integer)
-decodeOp x =
-    let toMode z =
-            case z of
-                0 -> Position
-                1 -> Immediate
-                2 -> Relative
-                _ -> error ("Bad mode! " ++ show z)
-    in ( toMode $ (x `div` 100) `mod` 10
-       , toMode $ (x `div` 1000) `mod` 10
-       , toMode $ (x `div` 10000) `mod` 10
-       , x `mod` 100)
-
-readR :: Mode -> Integer -> Map Integer Integer -> Integer -> Integer
-readR mode relativeBase insts value =
-    case mode of
-        Immediate -> value
-        Position -> fromMaybe 0 $ Map.lookup value insts
-        Relative -> fromMaybe 0 $ Map.lookup (relativeBase + value) insts
-
-writeR ::
-       Mode
-    -> Integer
-    -> Map Integer Integer
-    -> Integer
-    -> Integer
-    -> Map Integer Integer
-writeR mode relativeBase insts index value =
-    case mode of
-        Immediate -> error "Immediate write!"
-        Position -> Map.insert index value insts
-        Relative -> Map.insert (relativeBase + index) value insts
-
-step ::
-       (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-    -> (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-step state =
-    case state of
-        (_, _, Nothing, _, _) -> state
-        (relB, inputs, Just i, inst, outputs) ->
-            case ( decodeOp <$> Map.lookup i inst
-                 , fromMaybe 0 $ Map.lookup (i + 1) inst
-                 , fromMaybe 0 $ Map.lookup (i + 2) inst
-                 , fromMaybe 0 $ Map.lookup (i + 3) inst) of
-                (Just (ma, mb, mc, 1), a, b, c) ->
-                    ( relB
-                    , inputs
-                    , Just (i + 4)
-                    , writeR
-                          mc
-                          relB
-                          inst
-                          c
-                          (readR ma relB inst a + readR mb relB inst b)
-                    , outputs)
-                (Just (ma, mb, mc, 2), a, b, c) ->
-                    ( relB
-                    , inputs
-                    , Just (i + 4)
-                    , writeR
-                          mc
-                          relB
-                          inst
-                          c
-                          (readR ma relB inst a * readR mb relB inst b)
-                    , outputs)
-                (Just (ma, _, _, 3), a, _, _) ->
-                    case inputs of
-                        [] -> state
-                        (h:t) ->
-                            ( relB
-                            , t
-                            , Just (i + 2)
-                            , writeR ma relB inst a h
-                            , outputs)
-                (Just (ma, _, _, 4), a, _, _) ->
-                    ( relB
-                    , inputs
-                    , Just (i + 2)
-                    , inst
-                    , (readR ma relB inst a) : outputs)
-                (Just (ma, mb, _, 5), a, b, _) ->
-                    if readR ma relB inst a /= 0
-                        then ( relB
-                             , inputs
-                             , Just (readR mb relB inst b)
-                             , inst
-                             , outputs)
-                        else (relB, inputs, Just (i + 3), inst, outputs)
-                (Just (ma, mb, _, 6), a, b, _) ->
-                    if readR ma relB inst a == 0
-                        then ( relB
-                             , inputs
-                             , Just (readR mb relB inst b)
-                             , inst
-                             , outputs)
-                        else (relB, inputs, Just (i + 3), inst, outputs)
-                (Just (ma, mb, mc, 7), a, b, c) ->
-                    if readR ma relB inst a < readR mb relB inst b
-                        then ( relB
-                             , inputs
-                             , Just (i + 4)
-                             , writeR mc relB inst c 1
-                             , outputs)
-                        else ( relB
-                             , inputs
-                             , Just (i + 4)
-                             , writeR mc relB inst c 0
-                             , outputs)
-                (Just (ma, mb, mc, 8), a, b, c) ->
-                    if readR ma relB inst a == readR mb relB inst b
-                        then ( relB
-                             , inputs
-                             , Just (i + 4)
-                             , writeR mc relB inst c 1
-                             , outputs)
-                        else ( relB
-                             , inputs
-                             , Just (i + 4)
-                             , writeR mc relB inst c 0
-                             , outputs)
-                (Just (ma, _, _, 9), a, _, _) ->
-                    ( relB + readR ma relB inst a
-                    , inputs
-                    , Just (i + 2)
-                    , inst
-                    , outputs)
-                (Just (_, _, _, 99), _, _, _) ->
-                    (relB, inputs, Nothing, inst, outputs)
-                (Just x, _, _, _) -> error $ show (x, i, inst)
-                (_, _, _, _) -> error $ show (i, inst)
 
 answer1 :: _ -> _
 answer1 inst =
@@ -186,9 +46,7 @@ answer1 inst =
     fmap (\[x, y, _] -> (x, y)) $
     filter (\[_, _, x] -> x == 2) $
     chunksOf 3 $
-    reverse $
-    (\(_, _, _, _, o) -> o) $
-    until (\(_, _, a, _, _) -> a == Nothing) step (0, [], Just 0, inst, [])
+    toList $ snd $ snd $ consume $ (\x -> (x, mempty)) $ initialize inst
 
 data Tile
     = Wall
@@ -221,85 +79,47 @@ display' (score, m) ([x, y, 2]:t) =
 display' (score, m) ([x, y, 3]:t) =
     display' (score, Map.insert (x, y) Horizontal m) t
 display' (score, m) ([x, y, 4]:t) = display' (score, Map.insert (x, y) Ball m) t
-display' sm (_:[]) = sm
+display' sm [_] = sm
 
 display = display' (0, mempty)
 
-findBounce ::
-       (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-    -> (Integer, Integer)
-findBounce startingState =
-    (\(_, inp, _, _, (t:y:x:_)) -> (x, fromIntegral (5000 - length inp))) $
-    until
-        (\(_, _, x, _, c) ->
-             x == Nothing ||
-             case c of
-                 (t:y:x:pt:py:_) -> pt == 0 && t == 4 && y == 20 && py == 19
-                 _ -> False)
-        step $
-    (\(a, b, c, d, e) -> (a, take 5000 (repeat 0), c, d, e)) startingState
+-- Either Score (BallX, Inputs Count Needed)
+findBounce' :: Integer -> Computer -> Either Integer (Integer, Integer)
+findBounce' n startingState =
+    case consume (startingState, pure 0) of
+        (_, (Halt, o)) ->
+            let threes = chunksOf 3 $ toList o
+            in case filter (\[x, y, _] -> x == -1 && y == 0) threes of
+                   [[_, _, s]] -> Left s
+                   list -> error ("no score! " ++ show list)
+        (s, (Prompt, o)) ->
+            let threes = chunksOf 3 $ toList o
+            in case filter (\[_, y, t] -> y == 20 && t == 4) threes of
+                   [[x, _, _]] -> Right (x, n + 1)
+                   _ -> findBounce' (n + 1) s
 
-placePaddle ::
-       ( (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-       , Integer)
-    -> ( (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-       , Integer)
-placePaddle (startingState, x) =
-    let (targetX, steps) = findBounce startingState
-        changeSteps = abs (targetX - x)
+findBounce :: Computer -> Either Integer (Integer, Integer)
+findBounce = findBounce' 0
+
+placePaddle :: (Computer, Integer) -> (Integer, Integer) -> (Computer, Integer)
+placePaddle (startingState, x) (targetX, steps) =
+    let changeSteps = abs (targetX - x)
         newInputs =
             take
                 (fromIntegral (steps + 1))
-                ((take (fromIntegral changeSteps) $
-                  repeat
-                      (if targetX > x
-                           then 1
-                           else -1)) ++
+                (replicate
+                     (fromIntegral changeSteps)
+                     (if targetX > x
+                          then 1
+                          else -1) ++
                  repeat 0)
-    in ( until
-             (\(_, inp, x, _, c) ->
-                  x == Nothing ||
-                  case c of
-                      (t:y:x:pt:py:_) ->
-                          py == 20 &&
-                          pt == 0 && t == 4 && y == 19 && length inp == 0
-                      _ -> False)
-             step $
-         (\(a, b, c, d, e) -> (a, b ++ newInputs, c, d, e)) startingState
-       , targetX)
+    in (fst $ consume (startingState, Seq.fromList newInputs), targetX)
 
-run :: [Integer] -> Map Integer Integer -> String
-run input inst =
-    render $
-    display $
-    chunksOf 3 $
-    reverse $
-    (\(_, _, _, _, o) -> o) $
-    applyNTimes step (0, input, Just 0, Map.insert 0 2 inst, []) 512000
-
-start ::
-       Map Integer Integer
-    -> (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-start inst =
-    until
-        (\(_, x, _, _, _) -> length x == 0)
-        step
-        (0, [0], Just 0, Map.insert 0 2 inst, [])
-
-play ::
-       Integer
-    -> (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-    -> (Integer, [Integer], Maybe Integer, Map Integer Integer, [Integer])
-play paddlePos state =
-    fst $
-    until (\((_, _, x, _, _), _) -> x == Nothing) placePaddle (state, paddlePos)
+play :: (Computer, Integer) -> Integer
+play (state, x) =
+    case fmap (placePaddle (state, x)) $ findBounce state of
+        Left score -> score
+        Right x' -> play x'
 
 answer2 :: Map Integer Integer -> String
-answer2 inst =
-    render $
-    display $
-    chunksOf 3 $
-    reverse $
-    (\(_, _, _, _, o) -> o) $ -- play 21 $ start inst
-    until (\(a, b, c, d, e) -> c == Nothing) step $
-    fst $ applyNTimes placePaddle (start inst, 21) 218
+answer2 inst = show $ play (initialize (Map.insert 0 2 inst), 21)
---
+++ util/haskell/advent-of-code/src/AdventOfCode/IntCode.hs
@@ -0,0 +1,214 @@
+module AdventOfCode.IntCode
+    ( parseInsts
+    , step
+    , consume
+    , WaitState(..)
+    , Computer
+    , initialize
+    , stepN
+    ) where
+
+--import Prelude hiding (lookup)
+--import Data.ByteString (ByteString)
+import Data.Map (Map)
+
+import Data.List.Split (splitOn)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
+import Data.Sequence (Seq(..), (|>))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+
+listToIndexMap :: [a] -> Map Integer a
+listToIndexMap =
+    fst . foldl (\(m, i) v -> (Map.insert i v m, i + 1)) (mempty, 0)
+
+parseInsts :: String -> Map Integer Integer
+parseInsts = listToIndexMap . fmap read . splitOn ","
+
+data Mode
+    = Immediate
+    | Position
+    | Relative
+    deriving (Show)
+
+decodeOp :: Integer -> (Mode, Mode, Mode, Integer)
+decodeOp x =
+    let toMode z =
+            case z of
+                0 -> Position
+                1 -> Immediate
+                2 -> Relative
+                _ -> error ("Bad mode! " ++ show z)
+    in ( toMode $ (x `div` 100) `mod` 10
+       , toMode $ (x `div` 1000) `mod` 10
+       , toMode $ (x `div` 10000) `mod` 10
+       , x `mod` 100)
+
+readR :: Mode -> Integer -> Map Integer Integer -> Integer -> Integer
+readR mode relativeBase insts value =
+    case mode of
+        Immediate -> value
+        Position -> fromMaybe 0 $ Map.lookup value insts
+        Relative -> fromMaybe 0 $ Map.lookup (relativeBase + value) insts
+
+writeR ::
+       Mode
+    -> Integer
+    -> Map Integer Integer
+    -> Integer
+    -> Integer
+    -> Map Integer Integer
+writeR mode relativeBase insts index value =
+    case mode of
+        Immediate -> error "Immediate write!"
+        Position -> Map.insert index value insts
+        Relative -> Map.insert (relativeBase + index) value insts
+
+data WaitState
+    = Halt
+    | Prompt
+    deriving (Eq, Show)
+
+-- ( Relative Offset
+-- , Internal Input Buffer
+-- , PC (Nothing if Halted)
+-- , Self-Modifying Instruction Set
+-- )
+type Computer = (Integer, Seq Integer, Maybe Integer, Map Integer Integer)
+
+initialize :: Map Integer Integer -> Computer
+initialize m = (0, mempty, Just 0, m)
+
+-- (StartState and Maybe NewInputs) ->
+-- (NextState and Left means stop, Right means continue)
+step ::
+       (Computer, Maybe (Seq Integer))
+    -> (Computer, Either WaitState (Maybe Integer))
+step (s@(_, _, Nothing, _), Just _)
+  -- If we got an input, but the program was halted, drop the input and report Halt
+ = (s, Left Halt)
+step ((relB, inputs, pc, insts), Just newInputs)
+  -- Otherwise, if we got an input, bring it into the buffer and step again
+ = step ((relB, inputs <> newInputs, pc, insts), mempty)
+step (state, Nothing)
+  -- If we did not get an input and are not halted (attempt), proceed with the next instruction
+ =
+    case state of
+        (_, _, Nothing, _) -> (state, Left Halt)
+        (relB, inputs, Just i, insts) ->
+            case ( decodeOp <$> Map.lookup i insts
+                 , fromMaybe 0 $ Map.lookup (i + 1) insts
+                 , fromMaybe 0 $ Map.lookup (i + 2) insts
+                 , fromMaybe 0 $ Map.lookup (i + 3) insts) of
+                (Just (ma, mb, mc, 1), a, b, c) ->
+                    ( ( relB
+                      , inputs
+                      , Just (i + 4)
+                      , writeR
+                            mc
+                            relB
+                            insts
+                            c
+                            (readR ma relB insts a + readR mb relB insts b))
+                    , Right Nothing)
+                (Just (ma, mb, mc, 2), a, b, c) ->
+                    ( ( relB
+                      , inputs
+                      , Just (i + 4)
+                      , writeR
+                            mc
+                            relB
+                            insts
+                            c
+                            (readR ma relB insts a * readR mb relB insts b))
+                    , Right Nothing)
+                (Just (ma, _, _, 3), a, _, _) ->
+                    case inputs of
+                        Empty -> (state, Left Prompt)
+                        (h :<| t) ->
+                            ( (relB, t, Just (i + 2), writeR ma relB insts a h)
+                            , Right Nothing)
+                (Just (ma, _, _, 4), a, _, _) ->
+                    ( (relB, inputs, Just (i + 2), insts)
+                    , Right (Just (readR ma relB insts a)))
+                (Just (ma, mb, _, 5), a, b, _) ->
+                    (\s -> (s, Right Nothing)) $
+                    if readR ma relB insts a /= 0
+                        then (relB, inputs, Just (readR mb relB insts b), insts)
+                        else (relB, inputs, Just (i + 3), insts)
+                (Just (ma, mb, _, 6), a, b, _) ->
+                    (\s -> (s, Right Nothing)) $
+                    if readR ma relB insts a == 0
+                        then (relB, inputs, Just (readR mb relB insts b), insts)
+                        else (relB, inputs, Just (i + 3), insts)
+                (Just (ma, mb, mc, 7), a, b, c) ->
+                    (\s -> (s, Right Nothing)) $
+                    if readR ma relB insts a < readR mb relB insts b
+                        then ( relB
+                             , inputs
+                             , Just (i + 4)
+                             , writeR mc relB insts c 1)
+                        else ( relB
+                             , inputs
+                             , Just (i + 4)
+                             , writeR mc relB insts c 0)
+                (Just (ma, mb, mc, 8), a, b, c) ->
+                    (\s -> (s, Right Nothing)) $
+                    if readR ma relB insts a == readR mb relB insts b
+                        then ( relB
+                             , inputs
+                             , Just (i + 4)
+                             , writeR mc relB insts c 1)
+                        else ( relB
+                             , inputs
+                             , Just (i + 4)
+                             , writeR mc relB insts c 0)
+                (Just (ma, _, _, 9), a, _, _) ->
+                    ( ( relB + readR ma relB insts a
+                      , inputs
+                      , Just (i + 2)
+                      , insts)
+                    , Right Nothing)
+                (Just (_, _, _, 99), _, _, _) ->
+                    ((relB, inputs, Nothing, insts), Left Halt)
+                (Just x, _, _, _) -> error $ show (x, i, insts)
+                (_, _, _, _) -> error $ show (i, insts)
+
+consume' ::
+       Seq Integer
+    -> (Computer, Seq Integer)
+    -> (Computer, (WaitState, Seq Integer))
+consume' outs (state, inputs) =
+    case step (state, Just inputs) of
+        (nextState, Left r) -> (nextState, (r, outs))
+        (nextState, Right mOut) ->
+            consume'
+              -- If we got an output, append it
+                (maybe outs (\out -> outs |> out) mOut)
+                (nextState, mempty)
+
+consume :: (Computer, Seq Integer) -> (Computer, (WaitState, Seq Integer))
+consume = consume' mempty
+
+stepN' ::
+       Seq Integer
+    -> Integer
+    -> (Computer, Seq Integer)
+    -> (Computer, (Maybe WaitState, Seq Integer))
+stepN' outs 0 (state, inputs) = (state, (Nothing, outs))
+stepN' outs n (state, inputs) =
+    case step (state, Just inputs) of
+        (nextState, Left r) -> (nextState, (Just r, outs))
+        (nextState, Right mOut) ->
+            stepN'
+              -- If we got an output, append it
+                (maybe outs (\out -> outs |> out) mOut)
+                (n - 1)
+                (nextState, mempty)
+
+stepN ::
+       Integer
+    -> (Computer, Seq Integer)
+    -> (Computer, (Maybe WaitState, Seq Integer))
+stepN = stepN' mempty
