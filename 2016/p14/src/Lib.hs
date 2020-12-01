7babfcd196cb70ad72bb7d270eccc56225f08772
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-11-30 20:44:34 -0800

    2016 Day 14 now fully lazy--just absolutely slow as hell.

--- 2016/p14/src/Lib.hs
+++ 2016/p14/src/Lib.hs
@@ -4,6 +4,7 @@ import AdventOfCode.Util ()
 import qualified AdventOfCode.Util as Util
 import Control.Applicative ((<*>), (<|>), pure)
 import qualified Control.Applicative as App
+import Control.Monad (guard)
 import qualified Control.Monad.State.Lazy as Stae
 
 -- cryptonite is unfortunately slower!
@@ -58,42 +59,18 @@ repeats5 (a:b:c:d:e:t) =
         (repeats5 (b : c : d : e : t))
 repeats5 _ = []
 
-isKey :: Ord a => [[a]] -> Int -> Bool
-isKey xs i =
-    case (repeats3 (xs !! i)) of
-        [] -> False
-        threes ->
-            any
-                (\i' ->
-                     let fives = Set.fromList $ repeats5 (xs !! i')
-                     in (Set.size (Set.fromList threes `Set.intersection` fives) >
-                         0))
-                [i + 1 .. i + 1000]
-
-triples :: Ord a => IntMap [a] -> Int -> Set a
-triples xs i =
-    Set.fromList (List.take 1 (repeats3 (Maybe.fromJust (IntMap.lookup i xs))))
-
-quints :: Ord a => IntMap [a] -> Int -> Set a
-quints xs i = Set.fromList (repeats5 (Maybe.fromJust (IntMap.lookup i xs)))
-
-isKeyLazy :: Ord a => IntMap (Set a) -> IntMap (Set a) -> Int -> Bool
-isKeyLazy ts qs i =
-    let t = Maybe.fromJust (IntMap.lookup i ts)
-    in if Set.size t == 0
-           then False
-           else any (\q -> (Set.size (Set.intersection t q)) > 0) $
-                fmap
-                    (\i' -> Maybe.fromJust (IntMap.lookup i' qs))
-                    [i + 1 .. i + 1000]
+isKeyMonadic :: _ -> String -> [Int]
+isKeyMonadic h bs = do
+    i <- [0 ..]
+    let asHash = h bs i
+    three <- List.take 1 (repeats3 asHash)
+    j <- [i + 1 .. i + 1000]
+    guard (List.elem three (repeats5 (h bs j)))
+    return i
 
 -- 01:14:34.8
 answer1 :: _ -> _
-answer1 bs =
-    let xs = IntMap.fromAscList $ fmap (\i -> (i, hashIndex bs i)) [0 .. 30000]
-        ts = IntMap.fromAscList $ fmap (\i -> (i, triples xs i)) [0 .. 30000]
-        qs = IntMap.fromAscList $ fmap (\i -> (i, quints xs i)) [0 .. 30000]
-    in List.last $ List.take 64 $ List.filter (isKeyLazy ts qs) $ [0 .. 30000]
+answer1 bs = isKeyMonadic hashIndex bs !! 63
 
 stretchIndex :: String -> Int -> String
 stretchIndex bs i =
@@ -104,13 +81,7 @@ stretchIndex bs i =
 
 -- 01:38:34.267
 answer2 :: _ -> _
-answer2 bs =
-    let xs =
-            IntMap.fromAscList $
-            fmap (\i -> (i, stretchIndex bs i)) [0 .. 30000]
-        ts = IntMap.fromAscList $ fmap (\i -> (i, triples xs i)) [0 .. 30000]
-        qs = IntMap.fromAscList $ fmap (\i -> (i, quints xs i)) [0 .. 30000]
-    in List.last $ List.take 64 $ List.filter (isKeyLazy ts qs) $ [0 .. 30000]
+answer2 bs = isKeyMonadic stretchIndex bs !! 63
 
 show1 :: Show _a => _a -> String
 show1 = show
