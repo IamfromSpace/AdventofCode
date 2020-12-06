e34d39592a41f54d6a01a5b92ef16e3fab8aa975
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-12-05 21:31:49 -0800

    Add 2020 Day 6 in Haskell.

---
+++ 2020/p06/src/Lib.hs
@@ -0,0 +1,83 @@
+module Lib where
+
+import AdventOfCode.Util (multiLines)
+import qualified AdventOfCode.Util as Util
+import Control.Applicative ((<*>), (<|>), pure)
+import qualified Control.Applicative as App
+import qualified Control.Monad.State.Lazy as Stae
+import qualified Crypto.Hash.MD5 as MD5
+import qualified Data.Bits as Bits
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.ByteString.UTF8 ()
+import qualified Data.Char as Char
+import Data.Foldable (toList)
+import qualified Data.List as List
+import qualified Data.List.Split as Split
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import Data.Monoid (Sum(..))
+import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.String (fromString)
+import Prelude hiding ((++), init, lookup, map)
+import Text.Read (readMaybe)
+
+parse1 :: String -> [Set Char]
+parse1 = fmap (Set.fromList . List.concat) . multiLines
+
+parse2 :: String -> [[Set Char]]
+parse2 = fmap (fmap Set.fromList) . multiLines
+
+answer1 :: [Set Char] -> Int
+answer1 = List.sum . fmap Set.size
+
+xx :: [Set Char] -> Set Char -> Set Char
+xx (h:t) s = xx t (h `Set.intersection` s)
+xx [] s = s
+
+inters :: [Set Char] -> Set Char
+inters (h:t) = xx t h
+inters [] = mempty
+
+answer2 :: [[Set Char]] -> Int
+answer2 xs = List.sum $ fmap (Set.size . inters) xs
+
+show1 :: Show _a => _a -> String
+show1 = show
+
+show2 :: Show _a => _a -> String
+show2 = show
+
+ex1_1 :: _
+ex1_1 = undefined
+
+ex1_2 :: _
+ex1_2 = undefined
+
+ex1_3 :: _
+ex1_3 = undefined
+
+ex1_4 :: _
+ex1_4 = undefined
+
+ex1_5 :: _
+ex1_5 = undefined
+
+ex2_1 :: _
+ex2_1 = undefined
+
+ex2_2 :: _
+ex2_2 = undefined
+
+ex2_3 :: _
+ex2_3 = undefined
+
+ex2_4 :: _
+ex2_4 = undefined
+
+ex2_5 :: _
+ex2_5 = undefined
