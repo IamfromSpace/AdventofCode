fb9943b3b60ceb3f58142f1d3a49e840d519462b
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-12-14 08:59:43 -0800

    Clean-up 2020 Day 14 so that most operations are bitwise and part2 resuses part1 code.

--- 2020/p14/src/Lib.hs
+++ 2020/p14/src/Lib.hs
@@ -55,8 +55,8 @@ parse1 = fmap parseLine . lines
 parse2 :: String -> _
 parse2 = parse1
 
-maskToBits :: String -> (Int, Int)
-maskToBits m =
+mask1ToMaskFn :: String -> Int -> Int
+mask1ToMaskFn m =
     let ors =
             List.foldl' (\acc next -> acc * 2 + next) 0 $
             fmap
@@ -73,63 +73,40 @@ maskToBits m =
                          then 0
                          else 1)
                 m
-    in (ors, ands)
+    in ((.&.) ands) . ((.|.) ors)
 
-step :: Inst -> (String, Map Int Int) -> (String, Map Int Int)
-step inst (mask, mem) =
+step :: Inst -> (Int -> Int, Map Int Int) -> (Int -> Int, Map Int Int)
+step inst (maskFn, mem) =
     case inst of
-        Mask x -> (x, mem)
-        Mem to val ->
-            let (ors, ands) = maskToBits mask
-            in (mask, Map.insert to ((val .|. ors) .&. ands) mem)
+        Mask x -> (mask1ToMaskFn x, mem)
+        Mem to val -> (maskFn, Map.insert to (maskFn val) mem)
 
 answer1 :: [_] -> _
-answer1 = sum . Map.elems . snd . List.foldl' (flip step) ("", mempty)
+answer1 = sum . Map.elems . snd . List.foldl' (flip step) (id, mempty)
 
-show36Bit' :: Int -> Int -> String
-show36Bit' 36 x = []
-show36Bit' n x =
-    let (d, m) = x `divMod` 2
-    in (if m == 1
-            then '1'
-            else '0') :
-       show36Bit' (n + 1) d
-
-show36Bit :: Int -> String
-show36Bit = reverse . show36Bit' 0
-
-strMask :: String -> Int -> String
-strMask mask i =
-    List.zipWith
-        (\mC iC ->
-             case mC of
-                 '0' -> iC
-                 _ -> mC)
-        mask
-        (show36Bit i)
-
-maskToBits2 :: String -> [Int]
-maskToBits2 =
-    fmap (List.foldl' (\acc next -> acc * 2 + next) 0) .
+mask2ToMask1s :: String -> [String]
+mask2ToMask1s =
     sequence .
     fmap
         (\case
-             '1' -> [1]
-             '0' -> [0]
-             'X' -> [0, 1])
+             '1' -> ['1']
+             '0' -> ['X']
+             'X' -> ['0', '1'])
 
-mask2 :: String -> Int -> [Int]
-mask2 mask = maskToBits2 . strMask mask
+mask2ToMaskFns :: String -> [Int -> Int]
+mask2ToMaskFns = fmap (mask1ToMaskFn) . mask2ToMask1s
 
-step2 :: Inst -> (String, Map Int Int) -> (String, Map Int Int)
-step2 inst (mask, mem) =
+step2 :: Inst -> ([Int -> Int], Map Int Int) -> ([Int -> Int], Map Int Int)
+step2 inst (maskFns, mem) =
     case inst of
-        Mask x -> (x, mem)
+        Mask x -> (mask2ToMaskFns x, mem)
         Mem to val ->
-            (mask, List.foldl' (\m t -> Map.insert t val m) mem $ mask2 mask to)
+            ( maskFns
+            , List.foldl' (\m t -> Map.insert t val m) mem $
+              fmap (\f -> f to) maskFns)
 
 answer2 :: [Inst] -> Int
-answer2 = sum . Map.elems . snd . List.foldl' (flip step2) ("", mempty)
+answer2 = sum . Map.elems . snd . List.foldl' (flip step2) ([], mempty)
 
 show1 :: Show _a => _a -> String
 show1 = show
