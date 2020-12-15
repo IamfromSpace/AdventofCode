6449ff21f7e7c01ed175d60a59c143bb6f9f0c36
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-12-15 08:39:20 -0800

    Add 2020 Day 15 in Haskell.

---
+++ 2020/p15/src/Lib.hs
@@ -0,0 +1,92 @@
+module Lib where
+
+import AdventOfCode.Util (multiLines)
+import qualified AdventOfCode.Util as Util
+import Control.Applicative ((<*>), (<|>), pure)
+import qualified Control.Applicative as App
+import Control.Arrow
+       ((&&&), (***), (<+>), (<<<), (>>>), (>>^), (|||), arr)
+import qualified Control.Monad.State.Lazy as Stae
+import qualified Crypto.Hash.MD5 as MD5
+import qualified Data.Bits as Bits
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.ByteString.UTF8 ()
+import qualified Data.Char as Char
+import qualified Data.Either as Either
+import Data.Foldable (toList)
+import qualified Data.List as List
+import qualified Data.List.Split as Split
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import qualified Data.Maybe as Maybe
+import Data.Monoid (Sum(..))
+import Data.Sequence (Seq(..), (<|), (|>))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.String (fromString)
+import Prelude hiding ((++), init, lookup, map)
+import Text.ParserCombinators.PArrow (MD, (>>!))
+import qualified Text.ParserCombinators.PArrow as PA
+import Text.Read (readMaybe)
+
+parse1 :: String -> [Int]
+parse1 = fmap read . Split.splitOn ","
+
+parse2 :: String -> _
+parse2 = parse1
+
+play' :: Int -> Map Int Int -> Int -> [Int] -> [Int]
+play' i !m last (h:t) = h : play' (i + 1) (Map.insert last i m) h t
+play' i !m last [] =
+    let next =
+            case Map.lookup last m of
+                Just i' -> i - i'
+                Nothing -> 0
+    in next : play' (i + 1) (Map.insert last i m) next []
+
+play :: [Int] -> [Int]
+play = play' 0 mempty (-1)
+
+answer1 :: _ -> _
+answer1 xs = (play xs) !! 2019
+
+answer2 :: _ -> _
+answer2 xs = (play xs) !! (30000000 - 1)
+
+show1 :: Show _a => _a -> String
+show1 = show
+
+show2 :: Show _a => _a -> String
+show2 = show
+
+ex1_1 :: Int
+ex1_1 = 436
+
+ex1_2 :: _
+ex1_2 = undefined
+
+ex1_3 :: _
+ex1_3 = undefined
+
+ex1_4 :: _
+ex1_4 = undefined
+
+ex1_5 :: _
+ex1_5 = undefined
+
+ex2_1 :: _
+ex2_1 = undefined
+
+ex2_2 :: _
+ex2_2 = undefined
+
+ex2_3 :: _
+ex2_3 = undefined
+
+ex2_4 :: _
+ex2_4 = undefined
+
+ex2_5 :: _
+ex2_5 = undefined
