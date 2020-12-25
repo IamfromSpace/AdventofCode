0ff04e7b3c5b8cbcdd8fc0f9170c7fbc265b216e
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2020-12-24 21:22:42 -0800

    Add 2020 Day 25 in Haskell.

---
+++ 2020/p25/src/Lib.hs
@@ -0,0 +1,91 @@
+module Lib where
+
+import AdventOfCode.Util (multiLines)
+import qualified AdventOfCode.Util as Util
+import Control.Applicative ((<*>), (<|>), pure)
+import qualified Control.Applicative as App
+import Control.Arrow
+       ((&&&), (***), (<+>), (<<<), (>>>), (|||), arr)
+import qualified Control.Monad.State.Lazy as Stae
+import qualified Crypto.Hash.MD5 as MD5
+import qualified Data.Bits as Bits
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.ByteString.UTF8 ()
+import qualified Data.Char as Char
+import qualified Data.Either as Either
+import Data.Foldable (toList)
+import qualified Data.List as List
+import qualified Data.List.Split as Split
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import Data.Monoid (Sum(..))
+import Data.Sequence (Seq(..), (<|), (|>))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.String (fromString)
+import Prelude hiding ((++), init, lookup, map)
+import Text.ParserCombinators.PArrow (MD, (>>!))
+import qualified Text.ParserCombinators.PArrow as PA
+import Text.Read (readMaybe)
+
+parse1 :: String -> (Int, Int)
+parse1 s =
+    let [a, b] = fmap read $ lines s
+    in (a, b)
+
+parse2 :: String -> _
+parse2 = parse1
+
+loop :: Int -> Int -> Int
+loop sub i = (sub * i) `mod` 20201227
+
+getLoopNumber :: Int -> Int -> Int
+getLoopNumber sub target =
+    length $ takeWhile ((/=) target) $ iterate (loop sub) 1
+
+answer1 :: (Int, Int) -> _
+answer1 (card, door) =
+    let cardLoopNum = getLoopNumber 7 card
+    in iterate (loop door) 1 !! cardLoopNum
+
+answer2 :: _ -> _
+answer2 = id
+
+show1 :: Show _a => _a -> String
+show1 = show
+
+show2 :: Show _a => _a -> String
+show2 = show
+
+ex1_1 :: Int
+ex1_1 = 14897079
+
+ex1_2 :: _
+ex1_2 = undefined
+
+ex1_3 :: _
+ex1_3 = undefined
+
+ex1_4 :: _
+ex1_4 = undefined
+
+ex1_5 :: _
+ex1_5 = undefined
+
+ex2_1 :: _
+ex2_1 = undefined
+
+ex2_2 :: _
+ex2_2 = undefined
+
+ex2_3 :: _
+ex2_3 = undefined
+
+ex2_4 :: _
+ex2_4 = undefined
+
+ex2_5 :: _
+ex2_5 = undefined
