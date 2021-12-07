fef4ff9d37b1e6333ce55778b529e70ba357f2b2
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2021-12-06 21:16:54 -0800

    Add 2021 Day 7 in Haskell.

---
+++ 2021/p07/src/Lib.hs
@@ -0,0 +1,66 @@
+module Lib where
+
+import AdventOfCode.ArrowParser (APC, (!>>), (>>!))
+import qualified AdventOfCode.ArrowParser as AP
+import AdventOfCode.Util (multiLines)
+import qualified AdventOfCode.Util as Util
+import Control.Applicative (pure, (<*>), (<|>))
+import qualified Control.Applicative as App
+import Control.Arrow (arr, returnA, (&&&), (***), (<+>), (<<<), (>>>), (>>^), (|||))
+import qualified Control.Monad.State.Lazy as Stae
+import qualified Crypto.Hash.MD5 as MD5
+import qualified Data.Bits as Bits
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.ByteString.UTF8 ()
+import qualified Data.Char as Char
+import qualified Data.Either as Either
+import Data.Foldable (fold, toList)
+import Data.Group (invert, (~~))
+import qualified Data.List as List
+import qualified Data.List.Split as Split
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
+import Data.Monoid (Sum (..))
+import Data.Sequence (Seq (..), (<|), (|>))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.String (fromString)
+import Test.Hspec (describe, it, shouldBe)
+import Text.Read (readMaybe)
+import Prelude hiding (foldr, init, lookup, map, (++))
+
+parse1 :: String -> [Int]
+parse1 s = read ("[" <> s <> "]")
+
+parse2 :: String -> _
+parse2 = parse1
+
+diff t xs = sum $ fmap (\x -> abs (x - t)) xs
+
+answer1 :: _ -> _
+answer1 xs = fst $ head $ List.sort $ fmap (\t -> (diff t xs, t)) [minimum xs .. maximum xs]
+
+diff2 t xs = sum $ fmap (\x -> let n = abs (x - t) in (n * (n + 1)) `div` 2) xs
+
+answer2 :: _ -> _
+answer2 xs = fst $ head $ List.sort $ fmap (\t -> (diff2 t xs, t)) [minimum xs .. maximum xs]
+
+show1 :: Show _a => _a -> String
+show1 = show
+
+show2 :: Show _a => _a -> String
+show2 = show
+
+tests :: _
+tests = do
+  describe "pure components" $ do
+    it "should parse" $ answer1 [16, 1, 2, 0, 4, 2, 7, 1, 2, 14] `shouldBe` 2
+  describe "part 1" $ do
+    let p1 = Util.autoFileTest (answer1 . parse1)
+    it "example 1" $ p1 "./ex1_1.txt" undefined
+  describe "part 2" $ do
+    let p2 = Util.autoFileTest (answer2 . parse2)
+    it "example 1" $ p2 "./ex2_1.txt" undefined
