9ace96ed9e894f255fa84afeb0c30e760dc7b466
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2022-12-26 13:37:04 -0800

    Solve 2022 Day 4 part 1 with Clash (answered first on actual hardware).

--- 2022/p04/src/Aoc.hs
+++ 2022/p04/src/Aoc.hs
@@ -56,6 +56,63 @@ createDomain (knownVDomain @System){vName="Alchitry", vResetPolarity=ActiveLow, 
 
 createDomain (knownVDomain @System){vName="Alchitry3", vResetPolarity=ActiveLow, vPeriod=30000}
 
+
+data Parsed = Parsed
+  { next :: Vec 2 (Unsigned 4),
+    done :: Vec 6 (Unsigned 4)
+  }
+  deriving stock (Generic, Show, Eq, Ord)
+  deriving anyclass (NFData, NFDataX, ShowX)
+
+data Range a = Range a a
+  deriving stock (Generic, Show, Eq, Ord)
+  deriving anyclass (NFData, NFDataX, ShowX)
+
+data Pair a = Pair a a
+  deriving stock (Generic, Show, Eq, Ord)
+  deriving anyclass (NFData, NFDataX, ShowX)
+
+parsedToRangePair :: Parsed -> Pair (Range (Vec 2 (Unsigned 4)))
+parsedToRangePair (Parsed next done) =
+  Pair
+    (Range
+      (Vector.select (SNat :: SNat 0) (SNat :: SNat 1) (SNat :: SNat 2) done)
+      (Vector.select (SNat :: SNat 2) (SNat :: SNat 1) (SNat :: SNat 2) done)
+    )
+    (Range
+      (Vector.select (SNat :: SNat 4) (SNat :: SNat 1) (SNat :: SNat 2) done)
+      next
+    )
+
+parseT :: (Unsigned 2, Parsed) -> BitVector 8 -> ((Unsigned 2, Parsed), Maybe (Bool, Pair (Range (Vec 2 (Unsigned 4)))))
+parseT (finishedNumCount, p@Parsed { next, done }) char =
+  let
+    isDone = char == 4
+    isNumBreak = char == 10 || char == 45 || char == 44 || isDone
+    bcd = truncateB (unpack (char - 48) :: Unsigned 8)
+  in
+    if isNumBreak then
+      let parsed' = Parsed (repeat 0) (fst $ Vector.shiftInAtN done next)
+      in if finishedNumCount == 3 then
+            ((0, Parsed (repeat 0) (repeat 0)), Just (isDone, parsedToRangePair p))
+         else
+            ((finishedNumCount + 1, parsed'), Nothing)
+    else
+      ((finishedNumCount, Parsed (fst $ Vector.shiftInAtN next (bcd :> Nil)) done), Nothing)
+
+oneContainsOther :: Pair (Range (Vec 2 (Unsigned 4))) -> Bool
+oneContainsOther (Pair (Range a0 a1) (Range b0 b1)) =
+  -- This one is interesting in that we can pipeline it in 2 or even three
+  -- steps, however, because our final a && b || c && d fits perfectly on a
+  -- single LUT4, the benefits of two steps is minimal, and three is likely
+  -- harmful.  In the end, a two step pipeline exactly one LUT of propagation
+  -- time and no routing.  This is almost certainly not the bottleneck!
+  (a0 <= b0 && a1 >= b1) || (b0 <= a0 && b1 >= a1)
+
+sumT :: Unsigned 64 -> (Bool, Unsigned 64) -> (Unsigned 64, Maybe (Unsigned 64))
+sumT acc (True, new) = (0, Just (acc + new))
+sumT acc (False, new) = (acc + new, Nothing)
+
 bcd64T ::
   Maybe (Unsigned 64, Unsigned 64, Unsigned 6, Vec 20 (Unsigned 4), Vec 20 (Unsigned 4)) ->
   Maybe (Unsigned 64, Unsigned 64) ->
@@ -117,7 +174,7 @@ data BcdOrControl
 blankLeadingZeros :: KnownNat n => Vec n (Unsigned 4) -> Vec n (Maybe BcdOrControl)
 blankLeadingZeros v =
   case Vector.findIndex ((/=) 0) v of
-    Nothing -> Vector.replace (0 :: Int) (Just (Bcd 0)) $ repeat Nothing
+    Nothing -> Vector.replace (Vector.length v - 1) (Just (Bcd 0)) $ repeat Nothing
     Just i -> Vector.imap (\i' x -> if i' < i then Nothing else Just (Bcd x)) v
 
 bcdOrControlToAscii :: BcdOrControl -> BitVector 8
@@ -142,7 +199,14 @@ adapt m b ta =
 
 runCore1 :: HiddenClockResetEnable dom => Signal dom (Maybe (BitVector 8)) -> Signal dom (Maybe (Unsigned 64))
 runCore1 =
-  pure (pure (pure 0))
+  fmap join
+    . mealy (adapt sumT) 0
+    . register Nothing
+    . fmap (fmap (fmap (\x -> if x then 1 else 0)))
+    . fmap (fmap (fmap oneContainsOther))
+    . register Nothing
+    . fmap join
+    . mealy (adapt parseT) (0, Parsed (repeat 0) (repeat 0))
 
 runCore2 :: HiddenClockResetEnable dom => Signal dom (Maybe (BitVector 8)) -> Signal dom (Maybe (Unsigned 64))
 runCore2 =
@@ -203,14 +267,14 @@ charToUartRx char =
     ++ replicate (SNat :: SNat 16) 1
 
 testOutput1 :: Vec _ Char
-testOutput1 = $(listToVecTH "")
+testOutput1 = $(listToVecTH "2")
 
 testOutput2 :: Vec _ Char
-testOutput2 = $(listToVecTH "")
+testOutput2 = $(listToVecTH "0")
 
 testInput :: Vec _ Bit
 testInput =
-  let raw = $(listToVecTH "")
+  let raw = $(listToVecTH "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8")
    in ( (1 :> 1 :> 1 :> 1 :> Nil)
           ++ Vector.concatMap charToUartRx raw
           ++ charToUartRx (chr 4)
@@ -219,7 +283,7 @@ testInput =
 
 testInputCore :: Vec _ (Maybe (BitVector 8))
 testInputCore =
-  let raw = $(listToVecTH "")
+  let raw = $(listToVecTH "5-15,12-13\n10-11,3-5\n1-5,1-5")
    in ( (Nothing :> Nil)
           ++ fmap (Just . fromIntegral . ord) raw
           ++ (Just 4 :> Nothing :> Nil)
@@ -239,7 +303,7 @@ copyWavedrom =
       rst = resetGen :: Reset System
       inputSignal = stimuliGenerator clk rst testInputCore
       out = InOut <$> fmap BitsWave inputSignal <*> fmap ShowWave (exposeClockResetEnable runCore clk rst en inputSignal)
-   in setClipboard $ TL.unpack $ TLE.decodeUtf8 $ render $ wavedromWithClock 200 "" out
+   in setClipboard $ TL.unpack $ TLE.decodeUtf8 $ render $ wavedromWithClock 40 "" out
 
 testBench :: Signal Alchitry3 Bool
 testBench =
@@ -251,11 +315,11 @@ testBench =
         outputVerifier'
           clk
           rst
-          ( replicate (SNat :: SNat 999) 1
-              ++ replicate (SNat :: SNat 3200) 1 -- blank leading zeros
+          ( replicate (SNat :: SNat 7916) 1
+              ++ replicate (SNat :: SNat 3040) 1 -- blank leading zeros
               ++ Vector.concatMap charToUartRx testOutput1
               ++ charToUartRx '\n'
-              ++ replicate (SNat :: SNat 3200) 1 -- blank leading zeros
+              ++ replicate (SNat :: SNat 3040) 1 -- blank leading zeros
               ++ Vector.concatMap charToUartRx testOutput2
               ++ charToUartRx '\n'
               ++ charToUartRx (chr 4)
