a6a1641d36a65ffafb56d48e14f59b2457d737cf
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2022-12-28 19:48:23 -0800

    2022 Day 5 part 2 - parameterize our craneT function to prep for solving part 2.

--- 2022/p05/src/Aoc.hs
+++ 2022/p05/src/Aoc.hs
@@ -5,7 +5,7 @@ module Aoc where
 import Clash.Cores.UART (uartRx, uartTx)
 import Clash.Explicit.Reset (convertReset)
 import Clash.Explicit.Testbench (outputVerifier', stimuliGenerator, tbClockGen)
-import Clash.Prelude (Bit, BitVector, Clock, DomainPeriod, Enable, HiddenClockResetEnable, KnownNat, Reset, ResetPolarity (ActiveLow), SNat (SNat), Signal, System, Unsigned, bundle, createDomain, enableGen, exposeClockResetEnable, knownVDomain, mealy, register, repeat, replicate, resetGen, truncateB, unbundle, vName, vPeriod, vResetPolarity)
+import Clash.Prelude (Bit, BitVector, Clock, DomainPeriod, Enable, HiddenClockResetEnable, KnownNat, Reset, ResetPolarity (ActiveLow), SNat (SNat), Signal, System, Unsigned, bundle, createDomain, enableGen, exposeClockResetEnable, knownVDomain, mealy, pack, register, repeat, replicate, resetGen, truncateB, unbundle, vName, vPeriod, vResetPolarity)
 import Clash.Prelude.BlockRam (ResetStrategy (NoClearOnReset), blockRamU, readNew)
 import Clash.Sized.Vector (Vec (Nil, (:>)), listToVecTH, (!!), (++))
 import qualified Clash.Sized.Vector as Vector
@@ -138,16 +138,31 @@ parseT (MPP (PreToId count fromId)) x =
 parseT (MPP NeedNewLine) 10 = (MPP PreCount, Nothing)
 parseT s x = error ("Unexpected input while parsing.\n" <> show s <> "\n" <> show x)
 
-data CraneState = CraneState
+type CM9000Job = (Unsigned 8, Unsigned 4, Unsigned 4)
+
+type DoJob a = Vec 9 (Unsigned 8) -> Vec 9 (BitVector 8) -> a -> (Vec 9 (Unsigned 8), Vec 9 (Maybe (Unsigned 8, BitVector 8)), Maybe a)
+
+data CraneState a = CraneState
   { pointers :: Vec 9 (Unsigned 8),
-    job :: Maybe (Unsigned 8, Unsigned 4, Unsigned 4),
+    job :: Maybe a,
     isDone :: Bool
   }
   deriving stock (Generic, Eq, Show)
   deriving anyclass (NFData, NFDataX, ShowX)
 
-craneMoveHelper :: CraneState -> Vec 9 (BitVector 8) -> (CraneState, (Maybe (Vec 9 (BitVector 8)), Vec 9 (Unsigned 8), Vec 9 (Maybe (Unsigned 8, BitVector 8))))
-craneMoveHelper s@CraneState {pointers, job, isDone} readValues =
+startCM9000Job :: Unsigned 8 -> Unsigned 4 -> Unsigned 4 -> (Unsigned 8, Unsigned 4, Unsigned 4)
+startCM9000Job = (,,)
+
+doCM9000Job :: DoJob CM9000Job
+doCM9000Job pointers readValues (count, from, to) =
+  let fromPointer' = (pointers !! from) - 1
+      toPointer' = (pointers !! to) + 1
+      pointers' = Vector.replace from fromPointer' $ Vector.replace to toPointer' pointers
+      writeValues = Vector.replace to (Just (toPointer', readValues !! from)) $ repeat Nothing
+   in (pointers', writeValues, if count == 1 then Nothing else Just (count - 1, from, to))
+
+craneMoveHelper :: DoJob a -> CraneState a -> Vec 9 (BitVector 8) -> (CraneState a, (Maybe (Vec 9 (BitVector 8)), Vec 9 (Unsigned 8), Vec 9 (Maybe (Unsigned 8, BitVector 8))))
+craneMoveHelper doJob s@CraneState {pointers, job, isDone} readValues =
   case job of
     Nothing ->
       if isDone
@@ -156,49 +171,43 @@ craneMoveHelper s@CraneState {pointers, job, isDone} readValues =
             (Just readValues, repeat 0, repeat Nothing)
           )
         else (s, (Nothing, pointers, repeat Nothing))
-    Just (count, from, to) ->
-      let fromPointer' = (pointers !! from) - 1
-          toPointer' = (pointers !! to) + 1
-          pointers' = Vector.replace from fromPointer' $ Vector.replace to toPointer' pointers
-          writeValues = Vector.replace to (Just (toPointer', readValues !! from)) $ repeat Nothing
-          s' =
-            s
-              { pointers = pointers',
-                job = if count == 1 then Nothing else Just (count - 1, from, to)
-              }
-       in (s', (Nothing, pointers', writeValues))
+    Just job ->
+      let (pointers', writeValues, job') = doJob pointers readValues job
+       in ( s {pointers = pointers', job = job'},
+            (Nothing, pointers', writeValues)
+          )
 
-craneT :: CraneState -> (Maybe Inst, Vec 9 (BitVector 8)) -> (CraneState, (Maybe (Vec 9 (BitVector 8)), Vec 9 (Unsigned 8), Vec 9 (Maybe (Unsigned 8, BitVector 8))))
-craneT s (Just (Add n id), _) =
+craneT :: (Unsigned 8 -> Unsigned 4 -> Unsigned 4 -> a) -> DoJob a -> CraneState a -> (Maybe Inst, Vec 9 (BitVector 8)) -> (CraneState a, (Maybe (Vec 9 (BitVector 8)), Vec 9 (Unsigned 8), Vec 9 (Maybe (Unsigned 8, BitVector 8))))
+craneT _ _ s (Just (Add n id), _) =
   let idx = pointers s !! n
       idx' = idx - 1
       s' = s {pointers = Vector.replace n idx' (pointers s)}
    in (s', (Nothing, pointers s', Vector.replace n (Just (idx, id)) (repeat Nothing)))
-craneT s (Just InitialCratesDone, _) =
+craneT _ _ s (Just InitialCratesDone, _) =
   let s' = s {pointers = repeat 0}
    in (s', (Nothing, pointers s', repeat Nothing))
-craneT s (Just (Move count from to), readValues) =
-  craneMoveHelper (s {job = Just (count, from, to)}) readValues
-craneT s (Just CrateMovesDone, readValues) =
-  craneMoveHelper (s {isDone = True}) readValues
-craneT s (Nothing, readValues) =
-  craneMoveHelper s readValues
+craneT startJob doJob s (Just (Move count from to), readValues) =
+  craneMoveHelper doJob (s {job = Just (startJob count from to)}) readValues
+craneT _ doJob s (Just CrateMovesDone, readValues) =
+  craneMoveHelper doJob (s {isDone = True}) readValues
+craneT _ doJob s (Nothing, readValues) =
+  craneMoveHelper doJob s readValues
 
-runCore1 :: HiddenClockResetEnable dom => Signal dom (Maybe (BitVector 8)) -> Signal dom (Maybe (Vec 9 (BitVector 8)))
-runCore1 mByteSig =
-  let mParsed = join <$> mealy (adapt parseT) (SPP 0 Waiting) mByteSig
-      ramValues = bundle $ Vector.map (\i -> readNew (blockRamU NoClearOnReset (SNat :: SNat 256) undefined) (unbundle readAddrs !! i) (unbundle writes !! i)) $ Vector.iterate (SNat :: SNat 9) ((+) 1) (0 :: Unsigned 4)
-      (out, readAddrs, writes) = unbundle $ mealy craneT (CraneState (repeat 0) Nothing False) $ bundle (mParsed, ramValues)
+runCore1 :: HiddenClockResetEnable dom => Signal dom (Maybe Inst) -> Signal dom (Maybe (Vec 9 (BitVector 8)))
+runCore1 mParsed =
+  let ramValues = bundle $ Vector.map (\i -> readNew (blockRamU NoClearOnReset (SNat :: SNat 256) undefined) (unbundle readAddrs !! i) (unbundle writes !! i)) $ Vector.iterate (SNat :: SNat 9) ((+) 1) (0 :: Unsigned 4)
+      (out, readAddrs, writes) = unbundle $ mealy (craneT startCM9000Job doCM9000Job) (CraneState (repeat 0) Nothing False) $ bundle (mParsed, ramValues)
    in out
 
-runCore2 :: HiddenClockResetEnable dom => Signal dom (Maybe (BitVector 8)) -> Signal dom (Maybe (Unsigned 64))
+runCore2 :: HiddenClockResetEnable dom => Signal dom (Maybe Inst) -> Signal dom (Maybe (Vec 9 (BitVector 8)))
 runCore2 =
-  pure (pure (pure 0))
+  pure (pure (pure (fmap (truncateB . pack . ord) ('M' :> 'C' :> 'D' :> 'A' :> 'A' :> 'A' :> 'A' :> 'A' :> 'A' :> Nil))))
 
-runCore :: HiddenClockResetEnable dom => Signal dom (Maybe (BitVector 8)) -> Signal dom (Maybe (Vec 9 (BitVector 8), Unsigned 64))
-runCore maybeByteSig =
-  let part1 = runCore1 maybeByteSig
-      part2 = runCore2 maybeByteSig
+runCore :: HiddenClockResetEnable dom => Signal dom (Maybe (BitVector 8)) -> Signal dom (Maybe (Vec 9 (BitVector 8), Vec 9 (BitVector 8)))
+runCore mByteSig =
+  let mParsed = join <$> mealy (adapt parseT) (SPP 0 Waiting) mByteSig
+      part1 = runCore1 mParsed
+      part2 = runCore2 mParsed
       out = mealy awaitBothT (Nothing, Nothing) (bundle (part1, part2))
    in out
 
@@ -207,9 +216,9 @@ run =
   fst
     . uartTx (SNat :: SNat 2083333)
     . register Nothing
-    . mealy (delayBufferVecT (SNat :: SNat 160) (SNat :: SNat 11)) Nothing
+    . mealy (delayBufferVecT (SNat :: SNat 160) (SNat :: SNat 21)) Nothing
     . register Nothing
-    . fmap (fmap ((\x -> x ++ (10 :> 4 :> Nil)) . fst))
+    . fmap (fmap (\(a, b) -> a ++ (10 :> Nil) ++ b ++ (10 :> 4 :> Nil)))
     . runCore
     . register Nothing
     . uartRx (SNat :: SNat 2083333)
@@ -223,7 +232,7 @@ testOutput1 :: Vec _ Char
 testOutput1 = $(listToVecTH "CMZAAAAAA") -- Hove to cheat a bit
 
 testOutput2 :: Vec _ Char
-testOutput2 = $(listToVecTH "")
+testOutput2 = $(listToVecTH "MCDAAAAAA")
 
 testInput :: Vec _ Bit
 testInput =
@@ -270,6 +279,8 @@ testBench =
           rst
           ( replicate (SNat :: SNat 22727) 1
               ++ Vector.concatMap charToUartRx testOutput1
+              ++ charToUartRx '\n'
+              ++ Vector.concatMap charToUartRx testOutput2
               ++ charToUartRx '\n'
               ++ charToUartRx (chr 4)
               ++ (1 :> Nil)
